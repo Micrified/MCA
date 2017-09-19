b1b2d8115ef7983f58fbf1a0ba1692ed710c7e34
Jeroen van Straten <jeroen.van.straten@gmail.com>
2017-09-19 13:52:16 +0200

    Added some stuff to the after-update.sh script that disables broken repositories and dropbox

--- tools/scripts/after-update.sh
+++ tools/scripts/after-update.sh
@@ -1,3 +1,25 @@
 
+# Remove OpenSUSE update repositories now that they no longer work.
+if [ ! -f ~/.update-repos-removed ]; then
+  echo "Disabling broken software repositories..."
+  sudo zypper rr openSUSE-13.2-Debug
+  sudo zypper rr openSUSE-13.2-Update-Debug
+  sudo zypper rr openSUSE-13.2-Update-Debug-Non-Oss
+  sudo zypper rr openSUSE-13.2-Source
+  sudo zypper rr openSUSE-13.2-Update
+  sudo zypper rr openSUSE-13.2-Update-Non-Oss
+  touch ~/.update-repos-removed
+fi
+
+# Disable dropbox service now that we use a websocket connection for the boardserver.
+if [ ! -f ~/.dropbox-disabled ]; then
+  echo "Disabling dropbox..."
+  dropbox autostart n
+  dropbox stop
+  touch ~/.dropbox-disabled
+fi
+
 # Nothing here yet.
 echo "Everything up-to-date."
+
+
